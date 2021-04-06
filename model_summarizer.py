0e9eaf7faddecff317025bb67da55c895ab0b6d8
Vish <vsubramanian43@gatech.edu>
2021-04-05 22:13:39 -0700

    REST

--- model_summarizer.py
+++ model_summarizer.py
@@ -1,16 +1,3 @@
-# from transformers import pipeline
-
-# # Open and read the article
-# f = open("article.txt", "r", encoding="utf8")
-# to_tokenize = f.read()
-
-# # Initialize the HuggingFace summarization pipeline
-# summarizer = pipeline("summarization")
-# summarized = summarizer(to_tokenize, min_length=75, max_length=100)
-
-# # Print summarized text
-# print(summarized)
-
 from transformers import pipeline
 import pandas as pd
 import os
@@ -20,71 +7,67 @@ from mlflow.models import ModelSignature
 import json
 from mlflow.tracking import MlflowClient
 
+# Wrapper class for the sentiment analysis task pipeline using the boiler plate code
+
 
 class Summarizer(mlflow.pyfunc.PythonModel):
     '''
     Any MLflow Python model is expected to be loadable as a python_function model.
     '''
+
     def __init__(self):
-        from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+        from transformers import pipeline, AutoTokenizer, AutoModelWithLMHead
 
-        #self.summarize = pipeline('summarization',model="t5-small", tokenizer="t5-small")
-
-        tokenizer = AutoTokenizer.from_pretrained(
+        self.tokenizer = AutoTokenizer.from_pretrained(
             "deep-learning-analytics/wikihow-t5-small")
 
-        self.summarize = AutoModelForSeq2SeqLM.from_pretrained(
+        self.summarize = AutoModelWithLMHead.from_pretrained(
             "deep-learning-analytics/wikihow-t5-small")
-
-        
 
     def summarize_article(self, row):
-        # s = self.summarize(row['text'],min_length=75, max_length=500)[0]
-        s = self.summarize(row['text'], min_length=75, max_length=500)[0]
-        
+        tokenized_text = self.tokenizer.encode(row[0], return_tensors="pt")
+
+        # T5-small model trained on Wikihow All data set.
+        # model was trained for 3 epochs using a batch size of 16 and learning rate of 3e-4.
+        # Max_input_lngth is set as 512 and max_output_length is 150.
+        s = self.summarize.generate(
+            tokenized_text,
+            max_length=150,
+            num_beams=2,
+            repetition_penalty=2.5,
+            length_penalty=1.0,
+            early_stopping=True)
+
+        s = self.tokenizer.decode(s[0], skip_special_tokens=True)
         return [s]
 
     def predict(self, context, model_input):
-        #print('model_input=' + str(model_input), flush=True)
         model_input[['name']] = model_input.apply(
-            self.summarize_article, axis=1, result_type='expand')
+            self.summarize_article)
+
         return model_input
 
 
-
-
-
-
+# Input and Output formats
 input = json.dumps([{'name': 'text', 'type': 'string'}])
 output = json.dumps([{'name': 'text', 'type': 'string'}])
+# Load model from spec
 signature = ModelSignature.from_dict({'inputs': input, 'outputs': output})
 
-# experiment_name = "text_summarizer"
-# print("experiment_name:", experiment_name)
-# mlflow.set_experiment(experiment_name)
-# client = mlflow.tracking.MlflowClient()
-# experiment_id = client.get_experiment_by_name(experiment_name).experiment_id
-# print("experiment_id:", experiment_id)
-
-
-
+#MLFlow Operations
 mlflow.set_tracking_uri("")
 tracking_uri = mlflow.get_tracking_uri()
 print("Current tracking uri: {}".format(tracking_uri))
 
-
+# Start tracking
 with mlflow.start_run(run_name="hf_summarizer") as run:
     print(run.info.run_id)
-    print("mlflow models serve -m runs:/" + run.info.run_id + "/model --no-conda")
+    runner = run.info.run_id
+    print("mlflow models serve -m runs:/" +
+          run.info.run_id + "/model --no-conda")
     mlflow.pyfunc.log_model('model', loader_module=None, data_path=None, code_path=None,
                             conda_env=None, python_model=Summarizer(),
                             artifacts=None, registered_model_name=None, signature=signature,
                             input_example=None, await_registration_for=0)
 
-    #mlflow.register_model("runs:/" + run.info.run_id + "/model", "summarizer")
-
-# curl -X POST -H "Content-Type:application/json; format=pandas-split" --data '{"columns":["text"],"data":[["H.P.Lovecraft wrote his best books in Masachusettes."]]}' http://127.0.0.1: 5000/invocations
-
-# curl -X POST -H "Content-Type:application/json; format=pandas-split" --data '{"columns":["text"],"data":[@article.txt]}' http: // 127.0.0.1: 5000/invocations
-
-# mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlrun --host 0.0.0.0
+#curl -X POST -H "Content-Type:application/json; format=pandas-split" --data '{"columns":["text"],"data":[["H.P.Lovecraft wrote his best books in Masachusettes."]]}' http://127.0.0.1: 5000/invocations
--- pred.py
+++ pred.py
@@ -1,20 +1,13 @@
 import pandas as pd
 import mlflow
+from model_summarizer import runner
+from transformers import pipeline, AutoTokenizer, AutoModelWithLMHead
 
 
-# Python program to convert text
-# file to JSON
-
-
-import json
-
-
-# the file to be converted to
-# json format
+# Clean up text
 filename = 'article.txt'
 
-# dictionary where the lines from
-# text will be stored
+# dictionary where the lines from text will be stored
 dict1 = {}
 
 # first get all lines from file
@@ -33,26 +26,20 @@ with open(filename) as fh:
 
     for line in fh.readlines():
 
-
-    # reads each line and trims of extra the spaces
+        # reads each line and trims of extra the spaces
         # and gives only the valid words
-        #command, description = line.strip().split(None, 1)
 
         dict1["text"] = line.strip()
 
-#print(dict1)
-
-
-logged_model = 'file:///Users/vishwanath/Projects/hf_mlflow/mlruns/0/1149283738fe4a8e8af79e6fdac975e8/artifacts/model'
+#location of logged model artifacts
+logged_model = 'file:///Users/vishwanath/Projects/hf_mlflow/mlruns/0/' + \
+    runner + '/artifacts/model'
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.pyfunc.load_model(logged_model)
 
 
-
 # Predict on a Pandas DataFrame.
 summary = loaded_model.predict(pd.DataFrame(dict1, index=[0]))
-print(summary['name'][0]['summary_text'])
 
-
- 
+print(summary['name'][0])
